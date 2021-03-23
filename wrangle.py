d9554a4a6c0efcff6482b568f86dbb46e42a5bdd
Liam Jackson <Ljackson3613@yahoo.com>
2021-03-22 19:22:04 -0500

    wrangle.py

--- wrangle.py
+++ wrangle.py
@@ -40,8 +40,25 @@ def split_zillow(df):
 
     # split train_validate off into train (70% of 80% = 56%) and validate (30% of 80% = 24%)
     train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    
+    return train, validate, test
 
-        
+def split(df, stratify_by=''):
+    '''
+    this function takes in a dataframe and splits it into 3 samples, 
+    a test, which is 20% of the entire dataframe, 
+    a validate, which is 24% of the entire dataframe,
+    and a train, which is 56% of the entire dataframe. 
+    It then splits each of the 3 samples into a dataframe with independent variables
+    and a series with the dependent, or target variable. 
+    The function returns 3 dataframes and 3 series:
+    X_train (df) & y_train (series), X_validate & y_validate, X_test & y_test. 
+    '''
+    # split df into test (20%) and train_validate (80%)
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+
+    # split train_validate off into train (70% of 80% = 56%) and validate (30% of 80% = 24%)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
     # split train into X (dataframe, drop target) & y (series, keep target only)
     X_train = train.drop(columns=['taxvaluedollarcnt'])
     y_train = train['taxvaluedollarcnt']
@@ -55,3 +72,6 @@ def split_zillow(df):
     y_test = test['taxvaluedollarcnt']
     
     return X_train, y_train, X_validate, y_validate, X_test, y_test
+
+
+# taxvaluedollarcnt/taxamount
