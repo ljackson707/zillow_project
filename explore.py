3cfb95213537c722d5be0b14c256c46adb7d19ed
Liam Jackson <Ljackson3613@yahoo.com>
2021-03-24 07:45:05 -0500

    explore.py

--- explore.py
+++ explore.py
@@ -15,3 +15,20 @@ def explore_univariate(X_train, quant_vars):
         plt.show(p)
         print(descriptive_stats)
         
+def explore_univariate_quant(train, quant_var):
+    '''
+    takes in a dataframe and a quantitative variable and returns
+    descriptive stats table, histogram, and boxplot of the distributions. 
+    '''
+    descriptive_stats = train[quant_var].describe()
+    plt.figure(figsize=(8,2))
+
+    p = plt.subplot(1, 2, 1)
+    p = plt.hist(train[quant_var], color='lightseagreen')
+    p = plt.title(quant_var)
+
+    # second plot: box plot
+    p = plt.subplot(1, 2, 2)
+    p = plt.boxplot(train[quant_var])
+    p = plt.title(quant_var)
+    return p, descriptive_stats
